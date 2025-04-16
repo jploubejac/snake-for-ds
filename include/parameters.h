a2bf3a556f0128ddecf672990fd003cfc799817c
jploubejac <jeanphiloubejac@orange.fr>
2025-04-16 11:41:49 +0200

    add(parameters.h): set the rules of the snake jungle 🌴🐍

---
+++ include/parameters.h
@@ -0,0 +1,8 @@
+
+#define SNAKE_POS_MIN_X             0
+#define SNAKE_POS_MAX_X             20
+#define SNAKE_POS_MIN_Y             0
+#define SNAKE_POS_MAX_Y             10
+
+#define SNAKE_MAX_LENGTH        (SNAKE_MAY_X*SNAKE_MAX_Y)
+#define SNAKE_INIT_LENGTH       3 
