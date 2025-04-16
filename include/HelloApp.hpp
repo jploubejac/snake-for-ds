306bcfea1349ccad250aa1091c7184fb6271b32b
jploubejac <jeanphiloubejac@orange.fr>
2025-04-16 22:09:54 +0200

    delete: remove HelloApp class

--- include/HelloApp.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef HELLO_APP_HPP
-#define HELLO_APP_HPP
-
-class HelloApp{
-public:
-    void run();
-};
-
-#endif 
--- source/HelloApp.cpp
+++
@@ -1,9 +0,0 @@
-#include <nds.h>
-#include <stdio.h>
-#include "HelloApp.hpp"
-
-void HelloApp::run(){
-    consoleDemoInit();
-    iprintf("Hello from Nintendo DS++!");
-    while(1)swiWaitForVBlank();
-}
