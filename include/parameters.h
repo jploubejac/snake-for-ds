0dba1ac5e33543e36616ab58d1d43f950e6f3629
jploubejac <jeanphiloubejac@orange.fr>
2025-04-18 21:09:39 +0200

    feat: add sprite display parameters in parameters.h

--- include/parameters.h
+++ include/parameters.h
@@ -1,8 +1,21 @@
 
 #define SNAKE_POS_MIN_X             0
-#define SNAKE_POS_MAX_X             20
+#define SNAKE_POS_MAX_X             15
 #define SNAKE_POS_MIN_Y             0
-#define SNAKE_POS_MAX_Y             10
+#define SNAKE_POS_MAX_Y             11
 
-#define SNAKE_MAX_LENGTH        (SNAKE_POS_MAX_X*SNAKE_POS_MAX_Y)
-#define SNAKE_INIT_LENGTH       3 
+#define SNAKE_MAX_LENGTH            (SNAKE_POS_MAX_X*SNAKE_POS_MAX_Y)
+#define SNAKE_INIT_LENGTH           3 
+
+
+#define SCENE_GAME_TILE_HEAD_UP     0
+#define SCENE_GAME_TILE_HEAD_RIGHT  1
+#define SCENE_GAME_TILE_HEAD_DOWN   2
+#define SCENE_GAME_TILE_HEAD_LEFT   3
+#define SCENE_GAME_TILE_TAIL_DOWN   4
+#define SCENE_GAME_TILE_TAIL_LEFT   5
+#define SCENE_GAME_TILE_TAIL_UP     6
+#define SCENE_GAME_TILE_TAIL_RIGHT  7
+#define SCENE_GAME_TILE_BODY        8
+#define SCENE_GAME_TILE_FOOD        9
+#define SCENE_GAME_TILE_EMPTY       10
