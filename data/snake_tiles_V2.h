b51e004930eb803169889ace054a0244f4fdfcf6
jploubejac <jeanphiloubejac@orange.fr>
2025-04-18 21:07:37 +0200

    assets: add game sprites

---
+++ data/snake_tiles_V2.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(snake_tiles_V2)
+
+//======================================================================
+//
+//	snake_tiles_V2, 176x16@8, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 44 tiles not compressed
+//	Total size: 512 + 2816 = 3328
+//
+//	Time-stamp: 2025-04-18, 10:11:47
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SNAKE_TILES_V2_H
+#define GRIT_SNAKE_TILES_V2_H
+
+#define snake_tiles_V2TilesLen 2816
+extern const unsigned int snake_tiles_V2Tiles[704];
+
+#define snake_tiles_V2PalLen 512
+extern const unsigned short snake_tiles_V2Pal[256];
+
+#endif // GRIT_SNAKE_TILES_V2_H
+
+//}}BLOCK(snake_tiles_V2)
